ec3035b3ad893ead735e12ee00b7a53031f5665e
Andrew Dymond <andy.dymond@gmail.com>
2018-06-20 19:54:09 -0600

    Add header tests for input & clicks

--- __test__/header.test.js
+++ __test__/header.test.js
@@ -6,10 +6,48 @@ describe('header component', () => {
   let wrapper
 
   beforeEach(() => {
-    wrapper = <Header />
+    wrapper = shallow(<Header />)
   })
 
   it('should be a thing', () => {
+    expect(wrapper).toBeDefined()
+  })
+
+  it('should call submitIdea when button is clicked', () => {
+    wrapper.instance().submitIdea = jest.fn()
+
+    const titleInput = wrapper.find('input').first()
+    const bodyInput = wrapper.find('input').filterWhere(x => x.props().placeholder === "Body")
+    const submitButton = wrapper.find('button')
+
+    titleInput.simulate('change', { target: {value: 'title 1'}})
+    bodyInput.simulate('change', { target: {value: 'body 1'}})
+
+    submitButton.simulate('click')
+
+    expect(wrapper.instance().submitIdea).toHaveBeenCalled()
+    expect(wrapper.instance().submitIdea).toHaveBeenCalledTimes(1)
+  })
+
+  it('should call this.props.toDont and clear state fields', () => {
+    const mockFn = jest.fn()
+    wrapper = mount(<Header addToDont={ mockFn } />)
+
+    const titleInput = wrapper.find('input').first()
+    const bodyInput = wrapper.find('input').filterWhere(x => x.props().placeholder === 'Body')
+    const submitButton = wrapper.find('button')
+
+    titleInput.simulate('change', { target: { value: 'title 1'}})
+    bodyInput.simulate('change', { target: { value: 'body 1'}})
+
+    expect(wrapper.state().title).toEqual('title 1')
+    expect(wrapper.state().body).toEqual('body 1')
+
+    submitButton.simulate('click')
+
+    expect(wrapper.props().addToDont).toHaveBeenCalled()
+    expect(wrapper.state().title).toEqual('')
+    expect(wrapper.state().body).toEqual('')
 
   })
 })
